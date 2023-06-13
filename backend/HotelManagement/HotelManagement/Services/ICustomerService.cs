dcb03677cc6bf53753f9d8191cb1070eaa686307
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:47:13 +0200

    Create CustomerService

---
+++ backend/HotelManagement/HotelManagement/Services/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using HotelManagement.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Services
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> GetAllCustomers();
+        Task<Customer> GetCustomerById(int id);
+        Task<Customer> CreateCustomer(Customer customer);
+        Task<Customer> UpdateCustomer(Customer customer);
+        Task<Customer> DeleteCustomer(int id);
+    }
+}
