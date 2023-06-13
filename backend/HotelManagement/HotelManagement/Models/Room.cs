7c5e21a03640ba02f153d82214f6e1b5b6ba0734
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:48:05 +0200

    Create Room entity structure

---
+++ backend/HotelManagement/HotelManagement/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using HotelManagement.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace HotelManagement.Models
+{
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomId { get; set; }
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public RoomType RoomType { get; set; }
+        [Required]
+        public int MaxOccupancy { get; set; }
+        public int CurrOccupancy { get; set; }
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public RoomStatus RoomStatus { get; set; }
+
+    }
+}
