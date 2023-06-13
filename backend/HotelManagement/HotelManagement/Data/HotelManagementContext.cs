8bd643f75ed62e88cb165df8561ab91c15401a50
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:48:31 +0200

    Create HotelManagementContext.cs

---
+++ backend/HotelManagement/HotelManagement/Data/HotelManagementContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Data
+{
+    public class HotelManagementContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Booking> Bookings { get; set; } = null!;
+        public DbSet<BookingDetail> BookingDetails { get; set; } = null!;
+        public HotelManagementContext(DbContextOptions<HotelManagementContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>()
+                .HasKey(r => r.RoomId);
+        }
+    }
+}
