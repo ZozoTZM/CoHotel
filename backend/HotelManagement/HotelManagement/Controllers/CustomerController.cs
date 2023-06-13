8b8bb97ff6d5e1db76b4bb73e1472117e7d3540c
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:48:39 +0200

    Create CustomerController.cs

---
+++ backend/HotelManagement/HotelManagement/Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using HotelManagement.Models;
+using HotelManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Controllers
+{
+    [ApiController]
+    [Route("/customers")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAllCustomers()
+        {
+            var customers = await _customerService.GetAllCustomers();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        {
+            var customer = await _customerService.GetCustomerById(id);
+            if (customer == null)
+                return NotFound();
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateCustomer([FromBody] Customer customer)
+        {
+            var createdCustomer = await _customerService.CreateCustomer(customer);
+            return CreatedAtAction(nameof(GetCustomerById), new { id = createdCustomer.CustomerId }, createdCustomer);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Customer>> UpdateCustomer(int id, [FromBody] Customer customer)
+        {
+            if (id != customer.CustomerId)
+                return BadRequest();
+
+            var updatedCustomer = await _customerService.UpdateCustomer(customer);
+            return Ok(updatedCustomer);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            var deletedCustomer = await _customerService.DeleteCustomer(id);
+            if (deletedCustomer == null)
+                return NotFound();
+
+            return Ok(deletedCustomer);
+        }
+    }
+}
