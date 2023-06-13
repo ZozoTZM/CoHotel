82adaf1a64e438520513610df79bc109be5da26b
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:48:11 +0200

    Create Customer entity structure

---
+++ backend/HotelManagement/HotelManagement/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelManagement.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+
+        public ICollection<Booking> Bookings { get; set; } = null!;
+
+    }
+}
