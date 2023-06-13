230a4799e607b3dd8170648812cbc209a84494a5
ZozoTZM <torokzmark24@gmail.com>
2023-06-13 07:48:27 +0200

    Create BookingDetail entity structure

---
+++ backend/HotelManagement/HotelManagement/Models/BookingDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelManagement.Models
+{
+    public class BookingDetail
+    {
+       public int Id { get; set; }
+       public int RoomId { get; set; }
+       public Room Room { get; set; } = null!;
+       public int BookingId { get; set; }
+       public Booking Booking { get; set; } = null!;
+
+    }
+}
