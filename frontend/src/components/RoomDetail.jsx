8f3da0376c587e244960fe98ec6b77f93bf95f92
ZozoTZM <torokzmark24@gmail.com>
2023-06-19 10:18:23 +0200

    Create RoomDetail.jsx

---
+++ frontend/src/components/RoomDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RoomDetail = ({ date, roomNumber }) => {
+  // Implement the component logic and UI to display relevant information
+  return (
+    <div>
+      <h2>Room Detail</h2>
+      <p>Date: {date}</p>
+      <p>Room Number: {roomNumber}</p>
+      {/* Add more relevant information */}
+    </div>
+  );
+};
+
+export default RoomDetail;
