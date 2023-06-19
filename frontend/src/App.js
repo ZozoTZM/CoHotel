e3f05b79ee41751129b7bc6cb7df41e28ff3fb34
ZozoTZM <torokzmark24@gmail.com>
2023-06-19 10:17:46 +0200

    Add dashboard element to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import LandingPage from './components/landing-page/LandingPage';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
       <Routes>
           <Route path="/" element={<LandingPage handleLogin={handleLogin}/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </div>
   );
