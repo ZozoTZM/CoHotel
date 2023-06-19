9ada3ec1d552f53b3c921954c33a41b63c1a1f8a
ZozoTZM <torokzmark24@gmail.com>
2023-06-19 09:06:29 +0200

    Create BookingRequest helper class for incoming booking requests

---
+++ backend/HotelManagement/HotelManagement/Models/BookingRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManagement.Models
+{
+    public class BookingRequest
+    {
+        public Customer Customer { get; set; }
+        public DateTime BookingStart { get; set; }
+        public DateTime BookingEnd { get; set; }
+        public int? ThirdPartyId { get; set; }
+        public List<int> RoomNumbers { get; set; }
+    }
+}
