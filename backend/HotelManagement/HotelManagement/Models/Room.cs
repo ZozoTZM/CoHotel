00b381c0feb0a029a4132fbd4e4d58d1f1a23b9b
ZozoTZM <torokzmark24@gmail.com>
2023-06-23 08:44:48 +0200

    Update Room.cs

--- backend/HotelManagement/HotelManagement/Models/Room.cs
+++ backend/HotelManagement/HotelManagement/Models/Room.cs
@@ -7,10 +7,8 @@ namespace HotelManagement.Models
 {
     public class Room
     {
-        [Key]
-        public int RoomNumber { get; set; }
-
-        [Required]
+        public int RoomId { get; set; }
+        public string Identifier { get; set; }
         public RoomType RoomType { get; set; }
 
         [NotMapped] 
@@ -36,6 +34,10 @@ namespace HotelManagement.Models
 
         [Required]
         public RoomStatus RoomStatus { get; set; }
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; }
     }
 
 }
