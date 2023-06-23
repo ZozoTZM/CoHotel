2f2dd54f763ff7bfb89083f17c1ed28114fd44ea
ZozoTZM <torokzmark24@gmail.com>
2023-06-23 08:44:51 +0200

    Create Hotel.cs

---
+++ backend/HotelManagement/HotelManagement/Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManagement.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        // Other hotel properties
+
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
