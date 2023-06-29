0d7c1abc5b6755ad975fea51cdae8154a83b45e7
ZozoTZM <torokzmark24@gmail.com>
2023-06-30 00:18:00 +0200

    Update HotelManagementContext.cs

--- backend/HotelManagement/HotelManagement/Data/HotelManagementContext.cs
+++ backend/HotelManagement/HotelManagement/Data/HotelManagementContext.cs
@@ -8,10 +8,19 @@ namespace HotelManagement.Data
         public DbSet<Room> Rooms { get; set; } = null!;
         public DbSet<Customer> Customers { get; set; } = null!;
         public DbSet<Booking> Bookings { get; set; } = null!;
-        public DbSet<BookingDetail> BookingDetails { get; set; } = null!;
+        public DbSet<BookingRoom> BookingRooms { get; set; } = null!;
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<User> Users { get; set; }
         public HotelManagementContext(DbContextOptions<HotelManagementContext> options) : base(options)
         {
 
-        }      
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookingRoom>()
+                .HasNoKey(); 
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
