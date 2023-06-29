81d9d29c2850c60ad6f90e24043c8964ab8e5243
ZozoTZM <torokzmark24@gmail.com>
2023-06-30 00:17:03 +0200

    Update Program.cs
    Add Mapper configuration

--- backend/HotelManagement/HotelManagement/Program.cs
+++ backend/HotelManagement/HotelManagement/Program.cs
@@ -1,12 +1,15 @@
 using HotelManagement.Data;
 using HotelManagement.Services;
+using HotelManagement.DTOs;
+using HotelManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,16 +20,27 @@ builder.Services.AddDbContext<HotelManagementContext>(options =>
 builder.Services.AddScoped<IBookingService, BookingService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IRoomService, RoomService>();
-builder.Services.AddScoped<IBookingDetailService, BookingDetailService>();
 
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<CustomerDTO, Customer>();
+    cfg.CreateMap<BookingDTO, Booking>();
+    cfg.CreateMap<RoomDTO, Room>();
+});
 
+builder.Services.AddSingleton(mapperConfig.CreateMapper());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
-builder.Services.AddControllers();
-    
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.IgnoreNullValues = true;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
