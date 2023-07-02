c25a8158edbb97123c0a8f7878aac334c434e71a
ZozoTZM <torokzmark24@gmail.com>
2023-07-02 23:15:25 +0200

    Create RegisterDTO.cs

---
+++ backend/HotelManagement/HotelManagement/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagement.DTOs
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string HotelName { get; set; }
+    }
+
+}
