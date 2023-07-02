da6a8a5fe988ee87f0f9f4882104fcaff99c0e4b
ZozoTZM <torokzmark24@gmail.com>
2023-07-02 23:15:22 +0200

    Create AccountController.cs

---
+++ backend/HotelManagement/HotelManagement/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+        [ApiController]
+
+
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return Ok(); // Login successful
+            }
+            else
+            {
+                return BadRequest("Invalid login attempt."); // Login failed
+            }
+        }
+    }
+}
