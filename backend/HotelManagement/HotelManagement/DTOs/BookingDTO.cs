508aa6633e3b9b44acb1e2198c740a747e1caca2
ZozoTZM <torokzmark24@gmail.com>
2023-07-02 11:17:31 +0200

    Remove CustomerID from BookingDTO

--- backend/HotelManagement/HotelManagement/DTOs/BookingDTO.cs
+++ backend/HotelManagement/HotelManagement/DTOs/BookingDTO.cs
@@ -5,8 +5,7 @@
         public DateTime PlacedDate { get; set; }
         public DateTime BookingStart { get; set; }
         public DateTime BookingEnd { get; set; }
-        public int? ThirdPartyId { get; set; }
-        public int CustomerId { get; set; }
+        public int? ThirdPartyId { get; set; }       
         public List<int> RoomIdentifiers { get; set; }
     }
 
